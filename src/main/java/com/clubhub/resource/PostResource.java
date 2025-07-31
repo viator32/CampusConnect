d254ef9b7d792a0b864d2f1c6fd13f2e57062823
Dennis Flegler <dennis.flegler@tech11.com>
2025-07-31 14:40:23 +0200

    added missing import and null check

--- src/main/java/com/clubhub/resource/PostResource.java
+++ src/main/java/com/clubhub/resource/PostResource.java
@@ -9,9 +9,10 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.container.ContainerRequestContext;
 
 import com.clubhub.entity.dto.PostDTO;
 
@@ -20,27 +21,27 @@ import com.clubhub.entity.dto.PostDTO;
 @Consumes(MediaType.APPLICATION_JSON)
 public interface PostResource {
 
-    @GET
-    @Path("/clubs/{clubId}/posts")
-    List<PostDTO> getClubPosts(@PathParam("clubId") UUID clubId);
+	@GET
+	@Path("/clubs/{clubId}/posts")
+	List<PostDTO> getClubPosts(@PathParam("clubId") UUID clubId);
 
-    @POST
-    @Path("/clubs/{clubId}/posts")
-    Response create(@PathParam("clubId") UUID clubId, PostDTO postDTO, @Context ContainerRequestContext ctx);
+	@POST
+	@Path("/clubs/{clubId}/posts")
+	Response create(@PathParam("clubId") UUID clubId, PostDTO postDTO, @Context ContainerRequestContext ctx);
 
-    @POST
-    @Path("/posts/{postId}/like")
-    Response like(@PathParam("postId") UUID postId);
+	@POST
+	@Path("/posts/{postId}/like")
+	Response like(@PathParam("postId") UUID postId);
 
-    @POST
-    @Path("/posts/{postId}/bookmark")
-    Response bookmark(@PathParam("postId") UUID postId);
+	@POST
+	@Path("/posts/{postId}/bookmark")
+	Response bookmark(@PathParam("postId") UUID postId);
 
-    @POST
-    @Path("/posts/{postId}/share")
-    Response share(@PathParam("postId") UUID postId);
+	@POST
+	@Path("/posts/{postId}/share")
+	Response share(@PathParam("postId") UUID postId);
 
-    @GET
-    @Path("/users/{userId}/feed")
-    List<PostDTO> getFeed(@PathParam("userId") UUID userId);
+	@GET
+	@Path("/users/{userId}/feed")
+	List<PostDTO> getFeed(@PathParam("userId") UUID userId);
 }
