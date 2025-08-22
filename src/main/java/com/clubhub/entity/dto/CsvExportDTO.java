60d06456ca21c1b2701008ab955df76fe93a290c
Danosch <92167932+Danosch@users.noreply.github.com>
2025-08-22 22:47:19 +0200

    Use Lombok in CsvExportDTO

---
+++ src/main/java/com/clubhub/entity/dto/CsvExportDTO.java
@@ -0,0 +1,14 @@
+package com.clubhub.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CsvExportDTO {
+    private String fileName;
+    private String data;
+}
+
