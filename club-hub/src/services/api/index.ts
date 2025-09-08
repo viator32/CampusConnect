640fd96b0636404516df87bafd4bb424ed4cfd8e
serhiiradkovskyi <sergii2003rad@gmail.com>
2025-09-08 13:31:36 +0200

    some clean up

--- club-hub/src/services/api/index.ts
+++ club-hub/src/services/api/index.ts
@@ -1,6 +1,5 @@
 /**
  * API module barrel. Re-exports common API utilities and clients.
  */
-export { BaseApi } from './BaseApi';
 export { ApiError } from './ApiError';
 export { ClientApi, clientApi, setAuthToken } from './ClientApi';
